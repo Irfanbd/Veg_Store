b912682ef2891534e8061ddb99712677d21b9522
Irfanbd <gourob83@gmail.com>
2022-01-21 00:59:48 +0600

    update4.0

--- testimonial.js
+++ testimonial.js
@@ -11,35 +11,35 @@ const testimonials = [
     photo:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6',
     text:
-      "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
+      "First time in (RN) and YOU have to go! It’s the cutest little spot with amazing food. The (FN) is to die for. IT WAS FIRE!! The service we received was so amazing and we will definitely be back again. They made us feel welcomed and gave us an amazing experience.",
   },
   {
     name: 'June Cha',
     position: 'Software Engineer',
     photo: 'https://randomuser.me/api/portraits/women/44.jpg',
     text:
-      'This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!',
+      'This place is great! Atmosphere is chill and cool but the staff is also really friendly. They know what they’re doing and what they’re talking about, and you can tell making the customers happy is their main priority. Food is pretty good, some italian classics and some twists, and for their prices it’s 100% worth it.',
   },
   {
     name: 'Iida Niskanen',
     position: 'Data Entry',
     photo: 'https://randomuser.me/api/portraits/women/68.jpg',
     text:
-      "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
+      "Do yourself a favor and visit this lovely restaurant in (CN). The service is unmatched. The staff truly cares about your experience. The food is absolutely amazing – everything we tasted melted in other mouths. Absolutely the best meal we had while in (CN). Highly recommend!",
   },
   {
     name: 'Renee Sims',
     position: 'Receptionist',
     photo: 'https://randomuser.me/api/portraits/women/65.jpg',
     text:
-      "This guy does everything he can to get the job done and done right. This is the second time I've hired him, and I'll hire him again in the future.",
+      "When we think about celebrations, (RN) is always our first option and it never disappoints. From the starter to the dessert the quality and tastes of everything was outstanding, my (FN), (FN), (FN) might have been the best meal I have ever been served. Service was delightful and very professional. 10/10",
   },
   {
     name: 'Jonathan Nunfiez',
     position: 'Graphic Designer',
     photo: 'https://randomuser.me/api/portraits/men/43.jpg',
     text:
-      "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
+      " I have to say, I enjoyed every single bite of the meal in (RN). I had a 3 course meal, with a couple of beers. Considering the quality, the price is reasonable. Ideal for those who want a romantic night out. There was also plenty of room for bigger groups.",
   },
   {
     name: 'Sasha Ho',
@@ -47,14 +47,14 @@ const testimonials = [
     photo:
       'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb',
     text:
-      'This guy is a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!',
+      ' Excellent food. Menu is extensive and seasonal to a particularly high standard. Definitely fine dining. It can be expensive but worth it and they do different deals on different nights so it’s worth checking them out before you book. Highly recommended.',
   },
   {
     name: 'Veeti Seppanen',
     position: 'Director',
     photo: 'https://randomuser.me/api/portraits/men/97.jpg',
     text:
-      'This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.',
+      'We are so fortunate to have this place just a few minutes drive away from home. Food is stunning, both the tapas and downstairs restaurant. Cocktails wow, wine great and lovely selection of beers. Love this place and will continue to visit.',
   },
 ]
 
